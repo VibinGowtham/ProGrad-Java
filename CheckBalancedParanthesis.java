5ee299079687a3ebc166377423504b1aa1feddff
VibinGowtham <43290795+VibinGowtham@users.noreply.github.com>
2022-03-22 18:15:01 +0530

    Add files via upload

--- CheckBalancedParanthesis.java
+++ CheckBalancedParanthesis.java
@@ -2,30 +2,54 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class CheckBalancedParanthesis {
+    public static String addBrackets(String prefix,char middle,String suffix) {
+        String output="";
+        switch(middle){
+            case '}': {output=prefix+"{";break;}
+            case ')': {output=prefix+"(";break;}
+            case ']': {output=prefix+"[";break;}
+        }
+        return output+suffix;
+        
+    }
     public static void CheckValidBrackets(String input){
         Stack<Character> st= new Stack<>();
         for(int i=0;i<input.length();i++){
             if(input.charAt(i)=='(' || input.charAt(i)=='[' || input.charAt(i)=='{') st.push(input.charAt(i));
             else{
-                if(st.isEmpty()) return ;
-                switch(input.charAt(i)){
+                if(st.isEmpty())  {input=addBrackets(input.substring(0, i),input.charAt(i),input.substring(i));i++;}
+               else{
+                    switch(input.charAt(i)){
                     case '}':{
                            if(st.peek()=='{') {st.pop();continue;}
-                           else break ;
+                           else {
+                               input=addBrackets(input.substring(0, i),input.charAt(i),input.substring(i));
+                               i++;
+                               break;
+                            }
                     }
                     case ')':{
                         if(st.peek()=='(') {st.pop();continue;}
-                        else break ;
+                        else {
+                            input=addBrackets(input.substring(0, i),input.charAt(i),input.substring(i));
+                            // flag=0;
+                            i++;
+                            break;
+                         }
                  }
                  case ']':{
                     if(st.peek()=='[') {st.pop();continue;}
-                    else break;
+                    else {
+                        input=addBrackets(input.substring(0, i),input.charAt(i),input.substring(i));
+                        i++;
+                        break;
+                     }
              }
                 }
             }
-        }
+        }}
         if(st.isEmpty()){
-            System.out.println("Balanced"); 
+            System.out.println(input); 
         }
         else{
             while(!st.isEmpty()){
